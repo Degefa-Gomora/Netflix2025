7cd7ce9a4aa575afad19f1e724eb58e7e8cbf5c7
Degefa Gomora <degefag21@gmail.com>
2025-05-08 17:15:19 +0300

    Header and footer

--- netflix-2025/src/App.jsx
+++ netflix-2025/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 import './App.css'
+import Home from './Pages/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello react</h1>
+      <Home/>
     </>
   )
 }
