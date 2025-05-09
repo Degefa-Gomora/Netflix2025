c682c132e23d2e7991b1348293e51158b3d191d7
Degefa Gomora <degefag21@gmail.com>
2025-05-09 16:12:56 +0300

    Prepare Nflx-Clone-2025 for deployment

--- netflix-2025/vite.config.js
+++ netflix-2025/vite.config.js
@@ -7,6 +7,17 @@
 //   base: "/Nflx-Clone-2025/", // Replace with your new repo name
 // });
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/Nflx-Clone-2025/",
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
@@ -15,5 +26,5 @@ dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Nflx-Clone-2025/",
+  base: "/Nflx-Clone-2025/", // Matches homepage
 });
