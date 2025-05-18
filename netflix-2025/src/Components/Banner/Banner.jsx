4a5e500c573d24993787e74e214a70de39239664
Degefa Gomora <degefag21@gmail.com>
2025-05-18 07:15:08 +0300

    final commit

--- netflix-2025/src/Components/Banner/Banner.jsx
+++ netflix-2025/src/Components/Banner/Banner.jsx
@@ -36,7 +36,7 @@ function Banner() {
     <div
       className="banner"
       style={{
-        backgroundSize: "contain",
+        backgroundSize: "cover",
         backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
@@ -59,3 +59,167 @@ function Banner() {
 }
 
 export default Banner;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState, useEffect } from "react";
+// import axios from "../../utils/axios";
+// import requests from "../../utils/requests";
+// import { IoIosPlay } from "react-icons/io";
+// import { IoIosInformationCircleOutline } from "react-icons/io";
+
+// import "./Banner.css";
+
+// function Banner() {
+//   const [movie, setMovie] = useState({});
+
+//   useEffect(() => {
+//     async function fetchData() {
+//       try {
+//         const request = await axios.get(requests.fetchNetflixOriginals);
+//         setMovie(
+//           request.data.results[
+//             Math.floor(Math.random() * request.data.results.length)
+//           ]
+//         );
+//       } catch (error) {
+//         console.log("error", error);
+//       }
+//     }
+//     fetchData();
+//   }, []);
+
+//   const truncate = (str, n) =>
+//     str?.length > n ? str.substr(0, n - 1) + "..." : str;
+
+//   return (
+//     <div
+//       className="banner text-white d-flex align-items-end"
+//       style={{
+//         backgroundSize: "cover",
+//         backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+//         backgroundPosition: "center",
+//         backgroundRepeat: "no-repeat",
+//       }}
+//     >
+//       <div className="container py-5 banner_contents">
+//         <h1 className="banner_title fw-bold">
+//           {movie?.title || movie?.name || movie?.original_name}
+//         </h1>
+//         <div className="banner_buttons my-3">
+//           <button className="btn btn-light me-2">
+//             <IoIosPlay className="me-1" /> Play
+//           </button>
+//           <button className="btn btn-secondary">
+//             <IoIosInformationCircleOutline className="me-1" /> More Info
+//           </button>
+//         </div>
+//         <p className="banner_description lead">
+//           {truncate(movie?.overview, 150)}
+//         </p>
+//       </div>
+//       <div className="banner_fadebottom" />
+//     </div>
+//   );
+// }
+
+// export default Banner;
